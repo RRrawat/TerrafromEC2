9ccc0e5d1e9aaa5fcf8b55b78d34f0e2a6120c2a
RRrawat <81628422+RRrawat@users.noreply.github.com>
2021-11-09 18:36:36 +0530

    updated ec2

--- AWSEC2.tf
+++ AWSEC2.tf
@@ -1,52 +1,82 @@
+#MY FIRST EC2 TERRRAFORM FILE
+
 provider "aws" {
-    region = "us-east-1"
+  profile = "YOUR_AWS_PROFILE_NAME"
+  region  = "us-east-1"
 }
-resource "aws_instance" "terraform_ec2_example" {
-    ami = "ami-0ab4d1e9cf9a1215a"
-    instance_type = "t2.micro"
-    key_name = "Practic_key"
-    tags = {
-        Name = "Terraform_Practice"
-    }
 
-}
-resource "aws_default_vpc" "main_vpc" {
-    tags = {
-        Name = "main_vpc"
-    }
-}
-resource "aws_security_group" "TerraformEC2_SG" {
-  name        = "TerraformEC2_SG"
+
+resource "aws_security_group" "terraform-sg" {
+  name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_default_vpc.main_vpc.id
+    
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
 
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
   
   ingress {
-    description      = "Inbound rules from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = [aws_default_vpc.main_vpc.cidr_block]
-    
-  }
-  ingress {
-    description      = "Inbound rules from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = [aws_default_vpc.main_vpc.cidr_block]
-    
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
   }
 
+
+
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "TerraformEC2_SG"
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_instance" "terraform-ec2" {
+  ami = "MENTION_THE_AMI_ID"  #mention the os ami id
+  instance_type= "t2.micro"
+  key_name= "key_pair_name"   #use key pair name without .pem extension
+  security_groups= ["${aws_security_group.terraform-sg.name}"]
+  availability_zone = "us-east-2a"
+  user_data =   <<-EOF
+                #! /bin/bash
+                sudo apt-get update
+                sudo apt-get install -y apache2
+                sudo systemctl start apache2
+                sudo systemctl enable apache2
+                echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+                EOF
+  tags = {
+    Name = "Webserver"
+   }
+
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = "20"
+    iops = "100"
+    delete_on_termination = "true"
+ }
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.terraform-ec2.public_ip} > ip_add.txt"
   }
 }
